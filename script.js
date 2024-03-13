b9b661925bd71f70f83e4809d2103136992458a9
Rubi <rubidavilan@gmail.com>
2024-03-12 21:45:50 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//alert("Hola este es mi javascript");
+
+let nombre = "Rubi";
+nombre= "Talia"
+var nombre1 ="Rubi";
+const nombre2 = "Rubi";
+//nombre2="Kiara";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
+
+
+let contenidoTitulo= "Holi";
+let titulo = document.querySelector(".logo .fuente-acento");
+console.log(titulo);
+titulo.innerHTML= contenidoTitulo;
+
+//CONDICIONALES
+
+let textoTitulo =titulo.innerText;
+console.log(textoTitulo);
+
+if(textoTitulo=="Holi"){
+    titulo.innerHTML="Otro";
+} else {
+    console.log ("no se cumple")
+}
+
